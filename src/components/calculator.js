5ea984b8f6fa2cd4f31aa36f264fb7d1e65d41af
ragan <raganalemu3@gmail.com>
2023-05-29 21:51:10 +0300

    Add calculator

---
+++ src/components/calculator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Display from './display';
+
+const Calculator = () => (
+  <div className="calculator">
+    <Display />
+  </div>
+);
+
+export default Calculator;
