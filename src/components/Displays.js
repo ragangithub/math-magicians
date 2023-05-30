0191c88fa4983f558ea878408c1620d4f1a899e1
ragan <raganalemu3@gmail.com>
2023-05-30 13:23:22 +0300

    Add opertaion

--- src/components/Displays.js
+++ src/components/Displays.js
@@ -1,33 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
-const Displays = () => (
-  <>
-    <div className="display-container">
-      <div className="display display-2">1</div>
-    </div>
-    <div className="buttons">
-      <div className="button">AC</div>
-      <div className="button">+/-</div>
-      <div className="button">%</div>
-      <div className="button operator">/</div>
-      <div className="button">7</div>
+const Displays = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-      <div className="button">8</div>
-      <div className="button">9</div>
-      <div className="button operator">X</div>
-      <div className="button">4</div>
-      <div className="button">5</div>
-      <div className="button">6</div>
-      <div className="button operator">-</div>
-      <div className="button">1</div>
-      <div className="button">2</div>
-      <div className="button">3</div>
-      <div className="button operator">+</div>
-      <div className="button span">0</div>
-      <div className="button">.</div>
-      <div className="button operator">=</div>
-    </div>
-  </>
-);
+  const click = (btnName) => {
+    const newState = calculate(state, btnName);
+    setState(newState);
+  };
+
+  return (
+    <>
+      <div className="display-container">
+        <div className="display display-2">
+          {state.next || state.total || '0'}
+        </div>
+      </div>
+      <div className="buttons">
+        <button type="button" className="button" onClick={() => click('AC')}>
+          AC
+        </button>
+        <button type="button" className="button" onClick={() => click('+/-')}>
+          +/-
+        </button>
+        <button type="button" className="button" onClick={() => click('%')}>
+          %
+        </button>
+        <button
+          type="button"
+          className="button operator"
+          onClick={() => click('÷')}
+        >
+          /
+        </button>
+        <button type="button" className="button" onClick={() => click('7')}>
+          7
+        </button>
+
+        <button type="button" className="button" onClick={() => click('8')}>
+          8
+        </button>
+        <button type="button" className="button" onClick={() => click('9')}>
+          9
+        </button>
+        <button
+          type="button"
+          className="button operator"
+          onClick={() => click('x')}
+        >
+          X
+        </button>
+        <button type="button" className="button" onClick={() => click('4')}>
+          4
+        </button>
+        <button type="button" className="button" onClick={() => click('5')}>
+          5
+        </button>
+        <button type="button" className="button" onClick={() => click('6')}>
+          6
+        </button>
+        <button
+          type="button"
+          className="button operator"
+          onClick={() => click('-')}
+        >
+          -
+        </button>
+        <button type="button" className="button" onClick={() => click('1')}>
+          1
+        </button>
+        <button type="button" className="button" onClick={() => click('2')}>
+          2
+        </button>
+        <button type="button" className="button" onClick={() => click('3')}>
+          3
+        </button>
+        <button
+          type="button"
+          className="button operator"
+          onClick={() => click('+')}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="button span"
+          onClick={() => click('0')}
+        >
+          0
+        </button>
+        <button type="button" className="button" onClick={() => click('.')}>
+          .
+        </button>
+        <button
+          type="button"
+          className="button operator"
+          onClick={() => click('=')}
+        >
+          =
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Displays;
